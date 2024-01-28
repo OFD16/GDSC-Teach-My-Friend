963962251c2e150d032eb862dd5f0b5a3fd651fe
OFD16 <96663769+OFD16@users.noreply.github.com>
2024-01-28 19:07:36 +0300

    feat: app dir name updated and added firebase packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:Sharey/screens/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:shop_app/screens/splash/splash_screen.dart';
 
 import 'routes.dart';
 import 'theme.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -16,7 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'The Flutter Way - Template',
+      title: 'Sharey',
       theme: AppTheme.lightTheme(context),
       initialRoute: SplashScreen.routeName,
       routes: routes,
